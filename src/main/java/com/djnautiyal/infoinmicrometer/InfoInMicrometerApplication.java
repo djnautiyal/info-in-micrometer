7166113da0c7fb576b693125644bd0097c0d1cac
DJ Nautiyal <dhannanjainautiyal@gmail.com>
2020-09-13 11:04:41 +0530

    Initial commit

---
+++ src/main/java/com/djnautiyal/infoinmicrometer/InfoInMicrometerApplication.java
@@ -0,0 +1,13 @@
+package com.djnautiyal.infoinmicrometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoInMicrometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoInMicrometerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djnautiyal/infoinmicrometer/InfoInMicrometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djnautiyal.infoinmicrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoInMicrometerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
