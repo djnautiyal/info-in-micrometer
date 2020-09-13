b93fd4a0972f6438e6026192549ae90c2cb059eb
DJ Nautiyal <dhannanjainautiyal@gmail.com>
2020-09-13 17:56:41 +0530

    Added Metadata as tags in prometheus

--- src/main/java/com/djnautiyal/infoinmicrometer/metrics/ApplicationInfoProbe.java
+++ src/main/java/com/djnautiyal/infoinmicrometer/metrics/ApplicationInfoProbe.java
@@ -3,6 +3,7 @@ package com.djnautiyal.infoinmicrometer.metrics;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Tags;
 import io.micrometer.core.instrument.binder.MeterBinder;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -11,43 +12,44 @@ import java.net.UnknownHostException;
 
 public class ApplicationInfoProbe implements MeterBinder {
 
-    private final byte[] ip;
-    private String hostName;
+    private static final double UP = 1.0;
+    private static final double DOWN = 0.0;
+    private final String ip;
     private final String name;
-
+    private final String description;
+    private String hostName;
     @Value("${spring.application.name}")
     private String appName;
-
     @Value("${spring.application.id}")
     private String appId;
-
-    private final String description;
-    private static final double UP = 1.0;
     //private final Iterable<Tag> tags;
 
-    public ApplicationInfoProbe(){
-        InetAddress ip1 = null;
+    public ApplicationInfoProbe() {
+        InetAddress inetAddress = null;
         try {
-            ip1 = InetAddress.getLocalHost();
-        }catch(UnknownHostException e){
-            ip1 = null;
+            inetAddress = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            System.out.println("Unable to get Inetaddress");
         }
-        finally{
-            this.ip = ip1.getAddress();
-            this.hostName = ip1.getHostName();
-        }
+
+        this.ip = inetAddress == null ? null : inetAddress.getHostAddress();
+        this.hostName = inetAddress == null ? null : inetAddress.getHostName();
+
         this.name = "Application_Info";
         this.description = "Displaying Application Name, Id, Hostname, IP Address";
 
     }
+
     @Override
     public void bindTo(final MeterRegistry meterRegistry) {
-        Gauge.builder(this.name,this, value ->  UP )
+        Gauge.builder(this.name, this, value -> this.ip.isEmpty() ? UP : DOWN)
                 .description(this.description)
+                .tags(this.tags())
                 .register(meterRegistry);
     }
 
-//    protected static Iterable<Tag> tags(){
-//
-//    }
+    private Iterable<Tag> tags() {
+        return Tags.of(Tag.of("IpAddress", this.ip.toString()), Tag.of("HostName", this.hostName),
+                Tag.of("Application-Name", this.appName), Tag.of("Application-Id", this.appId));
+    }
 }
